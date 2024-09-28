70c24afd2baba5b843e7fee20d154495ff413c5b
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2024-09-28 11:07:59 -0700

    workflow + joystick complete, pre display

--- BlochBall.ino
+++ BlochBall.ino
@@ -8,22 +8,23 @@ const int Y_pin = A1; // analog pin connected to Y output
 
 // YELLOW
 const int X_PLUS_PIN = 11;
-//const int X_MINUS_PIN = 10;
+const int X_MINUS_PIN = 10;
+
+// RED
+const int Y_PLUS_PIN = 9;
+const int Y_MINUS_PIN = 8;
 
 // BLUE
-const int Z_ZERO_PIN = 12; 
-const int Z_ONE_PIN = 13;
+const int Z_ZERO_PIN = 13; 
+const int Z_ONE_PIN = 12;
 
 // Values for Bases
-const String Z_ZERO = "0";
-const String Z_ONE = "1";
-const String X_PLUS = "+";
-const String X_MINUS = "-";
-const String Y_PLUS = "i";
-const String Y_MINUS = "-i";
-
-const String VERTICAL[2] = {Z_ZERO, Z_ONE};
-const String HORIZONTAL[4] {X_PLUS, Y_PLUS, X_MINUS, Y_MINUS};
+const String Z_ZERO = "Z_ZERO";
+const String Z_ONE = "Z_ONE";
+const String X_PLUS = "X_PLUS";
+const String X_MINUS = "X_MINUS";
+const String Y_PLUS = "Y_PLUS";
+const String Y_MINUS = "Y_MINUS";
 
 // Values for navigation and calc
 String initialState = "";
@@ -42,19 +43,92 @@ String currentPosition = Z_ZERO;
 void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
+
   pinMode(Z_ZERO_PIN, OUTPUT);
   pinMode(Z_ONE_PIN, OUTPUT);
+
   pinMode(X_PLUS_PIN, OUTPUT);
-  // pinMode(X_MINUS_PIN, OUTPUT);
+  pinMode(X_MINUS_PIN, OUTPUT);
+
+  pinMode(Y_PLUS_PIN, OUTPUT);
+  pinMode(Y_MINUS_PIN, OUTPUT);
   Serial.begin(9600);
 
 
 }
-void performCalc() {
+String getIntraBasisConversionGate(String currState) {
+  if (currState[0] == "Z") {
+    return "X";
+  } else {
+    return "Z";
+  }
+}
+
+String performCalc() {
   Serial.print("performing calc\n ");
   Serial.print(initialState);
   Serial.print(finalState);
   Serial.print("\n");
+
+  char initialBasis = initialState[0];
+  char finalBasis = finalState[0];
+
+  String gatePattern = "";
+  String intermediateState = "";
+
+  // If initial state and final state aren't in the same basis, change initial state to being in the basis of final state
+  if (initialBasis != finalBasis) {
+    if ((initialBasis == "Z" && finalBasis == "X") || (initialBasis == "X" && finalBasis == "Z")) {
+      gatePattern = "H";
+      if (initialState.equals(Z_ZERO)) {
+        intermediateState = X_PLUS;
+      } else if (initialState.equals(Z_ONE)) {
+        intermediateState = X_MINUS;
+      } else if (initialState.equals(X_PLUS)) {
+        intermediateState = Z_ZERO;
+      } else {
+        intermediateState = Z_ONE;
+      }
+    } else if (initialBasis == "Z" && finalBasis == "Y") {
+      gatePattern = "SH";
+      if (initialState.equals(Z_ZERO)) {
+        intermediateState = Y_PLUS;
+      } else {
+        intermediateState = Y_MINUS;
+      }
+    } else if (initialBasis == "X" && finalBasis == "Y") {
+      gatePattern = "S";
+      if (initialState.equals(X_PLUS)) {
+        intermediateState = Y_PLUS;
+      } else {
+        intermediateState = Y_MINUS;
+      }
+    } else if (initialBasis == "Y" && finalBasis == "Z") {
+      gatePattern = "S†H";
+      if (initialState.equals(Y_PLUS)) {
+        intermediateState = Z_ZERO;
+      } else {
+        intermediateState = Z_ONE;
+      }
+    } else if (initialBasis == "Y" && finalBasis == "X") {
+      gatePattern = "S†";
+      if (initialState.equals(Y_PLUS)) {
+        intermediateState = X_PLUS;
+      } else {
+        intermediateState = X_MINUS;
+      }
+    }
+  } 
+
+  // If states still aren't the same, apply extra gate to flip state
+  if (!initialState.equals(finalState) || !intermediateState.equals(finalState)) {
+    if (finalBasis == "Z") {
+      gatePattern = "X" + gatePattern;
+    } else {
+      gatePattern = "Z" + gatePattern;
+    }
+  }
+  return gatePattern;
 }
 
 void handleVerticalChange(bool up) {
@@ -63,8 +137,10 @@ void handleVerticalChange(bool up) {
   } else if (currentPosition.equals(Z_ONE)) {
     up? currentPosition = X_PLUS: currentPosition = X_MINUS;
   } else {
-    up? currentPosition = Z_ZERO: currentPosition = Z_ONE;
+    up? currentPosition = Z_ONE: currentPosition = Z_ZERO;
   }
+  Serial.print(currentPosition);
+  Serial.print(" NEW POSITION\n");
 }
 
 void handleHorizontalChange(bool right) {
@@ -82,28 +158,17 @@ void handleHorizontalChange(bool right) {
   } else {
     right? currentPosition = Y_MINUS: currentPosition = Y_PLUS;
   }
+  Serial.print(currentPosition);
+  Serial.print(" NEW POSITION\n");
 }
 
 void setLight() {
-  if (currentPosition.equals(Z_ZERO)) {
-    digitalWrite(Z_ZERO_PIN, HIGH);
-    digitalWrite(Z_ONE_PIN, (gameState == 1 && initialState == Z_ONE? HIGH: LOW));
-    digitalWrite(X_PLUS_PIN, (gameState == 1 && initialState == X_PLUS? HIGH: LOW));
-  } else if (currentPosition.equals(Z_ONE)) {
-    digitalWrite(Z_ONE_PIN, HIGH);
-    digitalWrite(Z_ZERO_PIN, (gameState == 1 && initialState == Z_ZERO? HIGH: LOW));
-    digitalWrite(X_PLUS_PIN, (gameState == 1 && initialState == X_PLUS? HIGH: LOW));
-  } else if (currentPosition.equals(Y_PLUS)) {
-    Serial.print("Y PLUS \n");
-  } else if (currentPosition.equals(Y_MINUS)) {
-    Serial.print("Y MINUS \n");
-  } else if (currentPosition.equals(X_PLUS)) {
-    digitalWrite(X_PLUS_PIN, HIGH);
-    digitalWrite(Z_ZERO_PIN, (gameState == 1 && initialState == Z_ZERO? HIGH: LOW));
-    digitalWrite(Z_ONE_PIN, (gameState == 1 && initialState == Z_ONE? HIGH: LOW));
-  } else {
-    Serial.print("X MINUS \n");
-  }
+  digitalWrite(Z_ZERO_PIN, ((currentPosition.equals(Z_ZERO) || (gameState != 0 && initialState == Z_ZERO))? HIGH: LOW));
+  digitalWrite(Z_ONE_PIN, ((currentPosition.equals(Z_ONE) || (gameState != 0 && initialState == Z_ONE))? HIGH: LOW));
+  digitalWrite(X_PLUS_PIN, ((currentPosition.equals(X_PLUS) || (gameState != 0 && initialState == X_PLUS))? HIGH: LOW));
+  digitalWrite(X_MINUS_PIN, ((currentPosition.equals(X_MINUS) || (gameState != 0 && initialState == X_MINUS))? HIGH: LOW));
+  digitalWrite(Y_PLUS_PIN, ((currentPosition.equals(Y_PLUS) || (gameState != 0 && initialState == Y_PLUS))? HIGH: LOW));
+  digitalWrite(Y_MINUS_PIN, ((currentPosition.equals(Y_MINUS) || (gameState != 0 && initialState == Y_MINUS))? HIGH: LOW));
 }
 
 void logInfo() {
@@ -116,17 +181,15 @@ void logInfo() {
     finalState = currentPosition;
     gameState++;
     Serial.print("FINAL STATE DONE\n");
-  } else if (gameState == 2){
-    performCalc();
-    gameState++;
+    Serial.print(performCalc());
   } else {
     gameState = 0;
+    currentPosition = Z_ZERO;
   }
 }
 
 
 void loop() {
-  // Serial.print("Switch:  ");
   int press = digitalRead(SW_pin);
   int xCoord = analogRead(X_pin);
   int yCoord = analogRead(Y_pin);
@@ -135,47 +198,14 @@ void loop() {
 
   if (xCoord >= 600) {
     handleHorizontalChange(true);
-    // digitalWrite(Z_ZERO_PIN, HIGH);
-    // digitalWrite(Z_ONE_PIN, LOW);
-    // digitalWrite(X_PLUS_PIN, HIGH);
-    // digitalWrite(X_MINUS_PIN, HIGH);
-
   } else if (xCoord <= 400) {
     handleHorizontalChange(false);
-    // digitalWrite(Z_ZERO_PIN, HIGH);
-    // digitalWrite(Z_ONE_PIN, LOW);
-    // digitalWrite(X_PLUS_PIN, HIGH);
-
   } else if (yCoord >= 600) {
     handleVerticalChange(true);
-    // digitalWrite(Z_ZERO_PIN, LOW);
-    // digitalWrite(Z_ONE_PIN, HIGH);
-    // digitalWrite(X_PLUS_PIN, HIGH);
   } else if (yCoord <= 400) {
     handleVerticalChange(false);
-    // digitalWrite(Z_ZERO_PIN, LOW);
-    // digitalWrite(Z_ONE_PIN, HIGH);
-    // digitalWrite(X_PLUS_PIN, HIGH);
-
   } else if (press == 0) {
-    Serial.print("press detected\n");
-    // digitalWrite(Z_ONE_PIN, LOW);
-    // digitalWrite(Z_ZERO_PIN, HIGH);
-    // digitalWrite(X_PLUS_PIN, HIGH);
-
     logInfo();
   }
-  Serial.print(currentPosition);
-  Serial.print("\n");
-  // Serial.print("size");
-  // Serial.print(sizeof(gates)/sizeof(gates[0]));
-  // Serial.print(digitalRead(SW_pin));
-  // Serial.print("\n");
-  // Serial.print("X-axis: ");
-  // Serial.print(analogRead(X_pin));
-  // Serial.print("\n");
-  // Serial.print("Y-axis: ");
-  // Serial.println(analogRead(Y_pin));
-  // Serial.print("\n\n");
   delay(500);
 }
